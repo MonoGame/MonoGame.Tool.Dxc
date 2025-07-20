ed114896906e5d2b7c7b11eaf0459a88c8e4f400
Thomas Altenburger <thomas@flying-oak.com>
2025-07-20 22:27:27 +0200

    Trying to build for Windows

--- build/BuildWindowsTask.cs
+++ build/BuildWindowsTask.cs
@@ -1,3 +1,5 @@
+using Cake.Common.Tools.VSWhere.Latest;
+
 namespace BuildScripts;
 
 [TaskName("Build Windows")]
@@ -9,12 +11,56 @@ public sealed class BuildWindowsTask : FrostingTask<BuildContext>
 
     public override void Run(BuildContext context)
     {
+        var vswhere = new VSWhereLatest(context.FileSystem, context.Environment, context.ProcessRunner, context.Tools);
+
+        // Generate projects
+        string cmake = "cmake";
+
+        if (!IsOnPATH(cmake))
+            cmake = vswhere.Latest(new VSWhereLatestSettings()).FullPath + @"\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe";
+
         var buildWorkingDir = "cakebuild/";
         context.CreateDirectory(buildWorkingDir);
-        context.StartProcess("cmake", new ProcessSettings
+        context.StartProcess(cmake, new ProcessSettings
         {
             WorkingDirectory = buildWorkingDir,
             Arguments = "-C ../dxc/cmake/caches/PredefinedParams.cmake -DCMAKE_BUILD_TYPE=Release -G \"Visual Studio 17 2022\" ../dxc/"
         });
+
+        // Build
+        string msbuild = "msbuild";
+
+        if (!IsOnPATH(msbuild))
+            msbuild = vswhere.Latest(new VSWhereLatestSettings()).FullPath + @"\MSBuild\Current\Bin\MSBuild.exe";
+
+        context.StartProcess(msbuild, new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "LLVM.sln /p:Configuration=Release" });
+
+        // Copy artifact
+        context.CreateDirectory(context.ArtifactsDir);
+        context.CopyFile($"{buildWorkingDir}/bin/dxc-3.7", $"{context.ArtifactsDir}/dxc");
+    }
+
+    private bool IsOnPATH(string process)
+    {
+        if (string.IsNullOrEmpty(process))
+            return false;
+
+        if (!process.EndsWith(".exe"))
+            process += ".exe";
+
+        // Check if process exist on PATH env
+
+        var split = Environment.GetEnvironmentVariable("PATH")?.Split(';');
+        if (split != null)
+        {
+            foreach (var path in split)
+            {
+                string processPath = System.IO.Path.Combine(path, process);
+                if (File.Exists(processPath))
+                    return true;
+            }
+        }
+
+        return false;
     }
 }
