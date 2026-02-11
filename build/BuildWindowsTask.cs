904180d4246d851550b6651caa1b16499c4f9e33
Dean Ellis <dellis1972@googlemail.com>
2026-02-11 16:54:11 +0000

    Add Support for windows arm64 (#4)
    * Add Support for windows arm64
    * bump buildscripts

--- build/BuildWindowsTask.cs
+++ build/BuildWindowsTask.cs
@@ -17,11 +17,18 @@ public sealed class BuildWindowsTask : FrostingTask<BuildContext>
         var downloadUrl = "https://github.com/microsoft/DirectXShaderCompiler/releases/download/v1.8.2505.1/dxc_2025_07_14.zip";
         context.DownloadFile(downloadUrl, "cakebuild/dxc.zip");
         ZipFile.ExtractToDirectory("cakebuild/dxc.zip", $"cakebuild/");
-        context.CreateDirectory($"{context.ArtifactsDir}/bin/");
-        context.CopyFile($"cakebuild/bin/x64/dxc.exe", $"{context.ArtifactsDir}/bin/dxc.exe");
-        context.CopyFile($"cakebuild/bin/x64/dxcompiler.dll", $"{context.ArtifactsDir}/bin/dxcompiler.dll");
-        context.CopyFile($"cakebuild/LICENSE-LLVM.txt", $"{context.ArtifactsDir}/bin/LICENSE-LLVM.txt");
-        context.CopyFile($"cakebuild/LICENSE-MIT.txt", $"{context.ArtifactsDir}/bin/LICENSE-MIT.txt");
-        context.CopyFile($"cakebuild/LICENSE-MS.txt", $"{context.ArtifactsDir}/bin/LICENSE-MS.txt");
+        
+        BuildForArchitecture(context, "x64", "windows-x64");
+        BuildForArchitecture(context, "arm64", "windows-arm64");
+    }
+
+    private void BuildForArchitecture(BuildContext context, string arch, string rid)
+    {
+        context.CreateDirectory($"{context.ArtifactsDir}/{rid}");
+        context.CopyFile($"cakebuild/bin/{arch}/dxc.exe", $"{context.ArtifactsDir}/{rid}/dxc.exe");
+        context.CopyFile($"cakebuild/bin/{arch}/dxcompiler.dll", $"{context.ArtifactsDir}/{rid}/dxcompiler.dll");
+        context.CopyFile($"cakebuild/LICENSE-LLVM.txt", $"{context.ArtifactsDir}/{rid}/LICENSE-LLVM.txt");
+        context.CopyFile($"cakebuild/LICENSE-MIT.txt", $"{context.ArtifactsDir}/{rid}/LICENSE-MIT.txt");
+        context.CopyFile($"cakebuild/LICENSE-MS.txt", $"{context.ArtifactsDir}/{rid}/LICENSE-MS.txt");
     }
 }
